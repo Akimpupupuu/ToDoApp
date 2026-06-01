536a97b992531f44c0fb55605b29a91555f72be3
Akimpupupuu <dnsgon3@gmail.com>
2026-06-01 23:18:04 +0400

    working on logger and user service

---
+++ internal/features/users/transport/http/transport.go
@@ -0,0 +1,14 @@
+package http
+
+type UsersHTTPHandler struct {
+	usersService UsersService
+}
+
+type UsersService interface {
+}
+
+func NewUsersHTTPHandler(usersService UsersService) *UsersHTTPHandler {
+	return &UsersHTTPHandler{
+		usersService: usersService,
+	}
+}
